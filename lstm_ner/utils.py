a35d31310e640f2c87d307c469ba3b1ca771220d
Yago Tomé <yago.tome@hotmail.com>
2018-05-17 21:03:10 -0300

    Extract named entity from input file in read util fuction

--- lstm_ner/utils.py
+++ lstm_ner/utils.py
@@ -8,17 +8,21 @@ from unidecode import unidecode
 def read_input_file(filename: str):
     """
         Reads the input file and creates a list of sentences in which each sentence is a list of its word where the word
-        is a 2-dim tuple, whose elements are the word itself and its label (named entity), respectively.
+        is a 2-dim tuple, whose elements are the word itself and its label (named entity), respectively. Also creates
+        a map of label to index
 
         Expected files have a sequence of sentences. It has one word by line in first column (in a tab-separated file)
         followed in second column by its label, i.e., the named entity. The sentences are separated by an empty line.
 
         :param filename: Name of the file
+        :return: List of sentences, map of label to index
     """
     sentences = []
     sentence = []
-    with open(filename) as f:
-        for line in f:
+    label2idx = {'O': 0}
+    label_idx = 1
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
             line = line.strip()
             if line == "":
                 if len(sentence) > 0:
@@ -26,8 +30,13 @@ def read_input_file(filename: str):
                     sentence = []
                 continue
             splits = line.split('\t')
-            sentence.append((splits[0], splits[1]))
-    return sentences
+            word = splits[0]
+            label = splits[1]
+            sentence.append((word, label))
+            if label not in label2idx.keys():
+                label2idx[label] = label_idx
+                label_idx += 1
+    return sentences, label2idx
 
 
 def tokenizer_sentences(sentences: List[List[Tuple[str, str]]], word_indices: Dict[str, int],

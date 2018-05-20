6dbf2cd3c8c14c517a52d9c996c169a1c21793f3
Yago Tomé <yago.tome@hotmail.com>
2018-05-19 23:48:10 -0300

    Update model to use both word and char embeddings

--- lstm_ner/ner_model.py
+++ lstm_ner/ner_model.py
@@ -1,10 +1,10 @@
 from keras import Sequential
-from keras.layers import Embedding, LSTM, Dropout, Dense
+from keras.layers import Embedding, LSTM, Dropout, Dense, Merge, Reshape
 
 
-def generate_model(word_embeddings, lstm_units, num_labels, dropout_rate=.5):
+def generate_model(word_embedding_model, char_embedding_model, lstm_units, num_labels, dropout_rate=.5):
     model = Sequential()
-    model.add(word_embeddings)
+    model.add(Merge([word_embedding_model, char_embedding_model], mode='concat'))
     model.add(LSTM(lstm_units, return_sequences=True))
     model.add(Dropout(dropout_rate))
     model.add(LSTM(lstm_units))
@@ -19,3 +19,15 @@ def generate_embedding(input_length: int, weights=None, vocab_size=0, embedding_
         embedding_dim = weights.shape[1]
         return Embedding(vocab_size, embedding_dim, input_length=input_length, weights=[weights], trainable=False)
     return Embedding(vocab_size, embedding_dim, input_length=input_length)
+
+
+def generate_word_embedding_model(input_length: int, weights=None, vocab_size=0, embedding_dim=0):
+    model = Sequential()
+    model.add(generate_embedding(input_length, weights=weights, vocab_size=vocab_size, embedding_dim=embedding_dim))
+    return model
+
+
+def generate_char_embedding_model(input_length: int, weights=None, vocab_size=0, embedding_dim=0):
+    model = Sequential()
+    model.add(generate_embedding(input_length, weights=weights, vocab_size=vocab_size, embedding_dim=embedding_dim))
+    return model

a1b1f38b20c4da727318646075db6bfc6b1f4560
Yago Tomé <yago.tome@hotmail.com>
2018-05-24 13:21:08 -0300

    Save model as h5 file and load it when exists

--- lstm_ner/__main__.py
+++ lstm_ner/__main__.py
@@ -1,7 +1,18 @@
+import os
+
+from keras.models import load_model
 from keras.utils import np_utils
 
 from lstm_ner import utils
 from lstm_ner import ner_model as ner
+
+# defining constants
+word_embeddings_file = 'data/word_embeddings.txt'
+train_file = 'data/train_data.txt'
+test_file = 'data/test_data.txt'
+model_file = 'output/model.h5'
+char_embeddings_file = 'output/char-embeddings.txt'
+
 
 if __name__ == '__main__':
     # defining hyper parameters
@@ -14,39 +25,47 @@ if __name__ == '__main__':
     epochs = 100
 
     # loading data from files
-    embeddings_file = 'data/word_embeddings.txt'
-    word_embeddings, word2idx, char2idx = utils.read_embeddings_file(embeddings_file)
+    word_embeddings, word2idx, char2idx = utils.read_embeddings_file(word_embeddings_file)
     max_word_len = max(map(lambda word: len(word), word2idx.keys()))
-    train_file = 'data/train_data.txt'
     x_train, y_train, label2idx = utils.load_input_output_data(train_file, word2idx, word_window_size,
                                                                char2idx, max_word_len)
-    test_file = 'data/test_data.txt'
     x_test, y_test, _ = utils.load_input_output_data(test_file, word2idx, word_window_size,
                                                      char2idx, max_word_len)
-
-    # defining model
-    word_input_length = 2 * word_window_size + 1
-    max_word_len_padded = max_word_len + word_window_size * 2
     num_labels = len(label2idx)
-    word_embedding_model = ner.generate_word_embedding_model(word_input_length, weights=word_embeddings)
-    char_embedding_model = ner.generate_char_embedding_model(max_word_len, max_word_len_padded, word_input_length,
-                                                             char_embeddings_dim, conv_num, char_window_size,
-                                                             vocab_size=len(char2idx))
-    model = ner.generate_model(word_embedding_model, char_embedding_model, lstm_units, num_labels, dropout_rate)
-
-    # summarize the model
-    print(model.summary())
 
     # "binarize" labels
     y_train = np_utils.to_categorical(y_train, num_labels)
     y_test = np_utils.to_categorical(y_test, num_labels)
 
-    # training and eval
-    model.fit(x_train, y_train, epochs=epochs)
+    # load model whether it is saved
+    if os.path.exists(model_file):
+        model = load_model(model_file)
+        print(f'Model loaded from {model_file}')
+        print(model.summary())
+    else:
+        # defining model
+        word_input_length = 2 * word_window_size + 1
+        max_word_len_padded = max_word_len + word_window_size * 2
+        word_embedding_model = ner.generate_word_embedding_model(word_input_length, weights=word_embeddings)
+        char_embedding_model = ner.generate_char_embedding_model(max_word_len, max_word_len_padded, word_input_length,
+                                                                 char_embeddings_dim, conv_num, char_window_size,
+                                                                 vocab_size=len(char2idx))
+        model = ner.generate_model(word_embedding_model, char_embedding_model, lstm_units, num_labels, dropout_rate)
+
+        # summarize the model
+        print(model.summary())
+
+        # training model
+        model.fit(x_train, y_train, epochs=epochs)
+
+        # saving embeddings
+        embedding_layer = char_embedding_model.layers[0]
+        weights = embedding_layer.get_weights()[0]
+        utils.save_embeddings(char_embeddings_file, weights, char2idx)
+
+        # saving whole model
+        model.save(model_file)
+
+    # evaluating model
     loss, accuracy = model.evaluate(x_test, y_test)
     print('Accuracy: %f' % (accuracy * 100))
-
-    # saving embeddings
-    embedding_layer = char_embedding_model.layers[0]
-    weights = embedding_layer.get_weights()[0]
-    utils.save_embeddings('output/char-embeddings.txt', weights, char2idx)

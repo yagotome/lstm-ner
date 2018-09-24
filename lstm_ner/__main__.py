5bc0b2b3d902ee85b91978e235a3c07eb398d7c2
Yago Tomé <yago.tome@hotmail.com>
2018-09-23 23:40:40 -0300

    Label NN output according to thresh and refactoring

--- lstm_ner/__main__.py
+++ lstm_ner/__main__.py
@@ -1,5 +1,6 @@
 import os
-import time
+
+from functools import reduce
 
 from keras.models import load_model
 from keras.utils import np_utils
@@ -13,36 +14,31 @@ input_data_folder = 'data'
 model_file = 'output/model.h5'
 char_embeddings_file = 'output/char_embeddings.txt'
 
+# defining hyper parameters
+word_window_size = 5
+char_window_size = 5
+char_embeddings_dim = 20
+dropout_rate = 0.5
+lstm_units = 420
+conv_num = 10
+epochs = 50
+test_percent = 0.2
+not_entity_threshold = 0.7
 
-if __name__ == '__main__':
-    # defining hyper parameters
-    word_window_size = 5
-    char_window_size = 5
-    char_embeddings_dim = 20
-    dropout_rate = 0.5
-    lstm_units = 420
-    conv_num = 10
-    epochs = 50
-    test_percent = 0.2
 
+def main():
     # loading data from files
     word_embeddings, word2idx, char2idx = data_utils.read_embeddings_file(word_embeddings_file)
     max_word_len = max(map(lambda word: len(word), word2idx.keys()))
     train_data, test_data, label2idx = data_utils.load_dataset(input_data_folder, test_percent)
-    start_time = time.time()
     x_train, y_train = data_utils.transform_to_xy(train_data, word2idx, label2idx, word_window_size,
                                                   char2idx, max_word_len)
-    print('Elapsed1:', time.time() - start_time)
-    start_time = time.time()
     x_test, y_test = data_utils.transform_to_xy(test_data, word2idx, label2idx, word_window_size,
                                                 char2idx, max_word_len)
-    print('Elapsed2:', time.time() - start_time)
     num_labels = len(label2idx)
-
     # "binarize" labels
     y_train = np_utils.to_categorical(y_train, num_labels)
     y_test = np_utils.to_categorical(y_test, num_labels)
-
     # load model whether it is saved
     if os.path.exists(model_file):
         model = load_model(model_file)
@@ -71,7 +67,33 @@ if __name__ == '__main__':
 
         # saving whole model
         model.save(model_file)
-
     # evaluating model
     loss, accuracy = model.evaluate(x_test, y_test)
     print('Accuracy: %f' % (accuracy * 100))
+    output = model.predict(x_test)
+    labeled_output = label_output(output, label2idx, test_data)
+    for word, ent in labeled_output:
+        print(word + '\t' + ent)
+
+
+def label_output(output, label2idx, test_data):
+    classed_output = []
+    test_data_flat = reduce(lambda acc, cur: acc + cur, test_data, [])
+    for i in range(len(output)):
+        not_entity_idx = label2idx['O']
+        if output[i, not_entity_idx] >= not_entity_threshold:
+            entity = 'O'
+        else:
+            ent_prob_max = 0
+            ent_idx = not_entity_idx
+            for j, ent in enumerate(output[i]):
+                if ent > ent_prob_max and j != not_entity_idx:
+                    ent_prob_max = ent
+                    ent_idx = j
+            entity = [label for label, idx in label2idx.items() if idx == ent_idx][0]
+        classed_output.append((test_data_flat[i][0], entity))
+    return classed_output
+
+
+if __name__ == '__main__':
+    main()
